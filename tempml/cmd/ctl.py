e924f209658355f937519c5d32ee7c3a48c54a9a
Akira Yoshiyama <akirayoshiyama@gmail.com>
2017-06-06 13:28:11 +0900

    ctl: created
    tempmlctl is a management tool for TempML. Currently, it's the only
    tool to add/update tenants.

---
+++ tempml/cmd/ctl.py
@@ -0,0 +1,290 @@
+# Copyright 2017 by Akira Yoshiyama <akirayoshiyama@gmail.com>.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+Manager command
+"""
+
+import click
+import email_normalize
+import logging
+import os
+import pbr.version
+from pprint import pprint
+import sys
+import yaml
+
+
+from tempml import const
+from tempml import db
+from tempml import log
+
+
+CONFIG_FILE = "/etc/tempml/tempml.conf"
+ERROR_RETURN = 'tempml-error'
+
+
+def normalize(addresses):
+    logging.debug(addresses)
+    result = []
+    for address in addresses:
+        try:
+            cleaned = email_normalize.normalize(address, resolve=False)
+            if isinstance(cleaned, str):
+                result.append(cleaned)
+        except:
+            pass
+    return set(result)
+
+
+@click.group()
+@click.option('--config-file', metavar='CONF',
+              envvar='TEMPML_CONFIG_FILE', type=click.File('r'),
+              help='Configuration File')
+@click.option('--debug', metavar='False',
+              envvar='TEMPML_DEBUG', is_flag=True,
+              help='Debug mode')
+@click.pass_context
+def cli(ctx, config_file, debug):
+    config = {}
+    if config_file is None:
+        config_file = open(CONFIG_FILE)
+    config = yaml.load(config_file.read())
+    logging.debug("config: %s", config)
+    log.setup(debug=debug)
+    ctx.obj = {
+        'config': config,
+        'debug': debug,
+    }
+
+
+@cli.group('tenant', help='Tenant operations')
+@click.pass_context
+def tenant(ctx):
+    pass
+
+
+@tenant.command('create', help='Register parameters of a tenant')
+@click.argument('name')
+@click.option('--yamlfile', type=click.File('r', encoding='utf-8'))
+@click.option('--admin', multiple=True)
+@click.option('--charset')
+@click.option('--enable', is_flag=True)
+@click.option('--disable', is_flag=True)
+@click.option('--days-to-close', type=int)
+@click.option('--days-to-orphan', type=int)
+@click.option('--ml-name-format')
+@click.option('--new-ml-account')
+@click.option('--welcome-file', type=click.File('r', encoding='utf-8'))
+@click.option('--readme-file', type=click.File('r', encoding='utf-8'))
+@click.option('--remove-file', type=click.File('r', encoding='utf-8'))
+@click.option('--reopen-file', type=click.File('r', encoding='utf-8'))
+@click.option('--goodbye-file', type=click.File('r', encoding='utf-8'))
+@click.option('--report-subject')
+@click.option('--report-file', type=click.File('r', encoding='utf-8'))
+@click.option('--report-format')
+@click.option('--orphaned-subject')
+@click.option('--orphaned-file', type=click.File('r', encoding='utf-8'))
+@click.option('--closed-subject')
+@click.option('--closed-file', type=click.File('r', encoding='utf-8'))
+@click.pass_context
+def create_tenant(ctx, name, yamlfile, admin, charset, enable, disable,
+                  days_to_close, days_to_orphan,
+                  ml_name_format, new_ml_account,
+                  welcome_file, readme_file, remove_file, reopen_file,
+                  goodbye_file,
+                  report_subject, report_file, report_format,
+                  orphaned_subject, orphaned_file,
+                  closed_subject, closed_file):
+    tenant_config = {}
+    if yamlfile:
+        tenant_config = yaml.load(yamlfile.read())
+    if len(admin) > 0:
+        tenant_config['admins'] = set(admin)
+    if charset is not None:
+        tenant_config['charset'] = charset
+    if enable is True:
+        tenant_config['status'] = const.TENANT_STATUS_ENABLED
+    elif disable is True:
+        tenant_config['status'] = const.TENANT_STATUS_DISABLED
+    if days_to_close is not None:
+        tenant_config['days_to_close'] = days_to_close
+    if days_to_orphan is not None:
+        tenant_config['days_to_orphan'] = days_to_orphan
+    if ml_name_format is not None:
+        tenant_config['ml_name_format'] = ml_name_format
+    if new_ml_account is not None:
+        tenant_config['new_ml_account'] = new_ml_account
+    if welcome_file is not None:
+        tenant_config['welcome_msg'] = welcome_file.read()
+    if readme_file is not None:
+        tenant_config['readme_msg'] = readme_file.read()
+    if remove_file is not None:
+        tenant_config['remove_msg'] = remove_file.read()
+    if goodbye_file is not None:
+        tenant_config['goodbye_msg'] = goodbye_file.read()
+    if reopen_file is not None:
+        tenant_config['reopen_msg'] = reopen_file.read()
+    if report_subject is not None:
+        tenant_config['report_subject'] = report_subject
+    if report_file is not None:
+        tenant_config['report_msg'] = report_file.read()
+    if report_format is not None:
+        tenant_config['report_format'] = report_format
+    if orphaned_subject is not None:
+        tenant_config['orphaned_subject'] = orphaned_subject
+    if orphaned_file is not None:
+        tenant_config['orphaned_msg'] = orphaned_file.read()
+    if closed_subject is not None:
+        tenant_config['closed_subject'] = closed_subject
+    if closed_file is not None:
+        tenant_config['closed_msg'] = closed_file.read()
+
+    logging.debug("tenant_name: %s", name)
+    logging.debug("tenant_config: %s", tenant_config)
+    config = ctx.obj['config']
+    db.init_db(config['db_url'],  config['db_name'])
+    db.create_tenant(name, "CLI", tenant_config)
+    return 0
+
+
+@tenant.command('update', help='Update parameters of a tenant')
+@click.argument('name')
+@click.option('--yamlfile', type=click.File('r', encoding='utf-8'))
+@click.option('--admin', multiple=True)
+@click.option('--charset')
+@click.option('--enable', is_flag=True)
+@click.option('--disable', is_flag=True)
+@click.option('--days-to-close', type=int)
+@click.option('--days-to-orphan', type=int)
+@click.option('--ml-name-format')
+@click.option('--new-ml-account')
+@click.option('--welcome-file', type=click.File('r', encoding='utf-8'))
+@click.option('--readme-file', type=click.File('r', encoding='utf-8'))
+@click.option('--remove-file', type=click.File('r', encoding='utf-8'))
+@click.option('--reopen-file', type=click.File('r', encoding='utf-8'))
+@click.option('--goodbye-file', type=click.File('r', encoding='utf-8'))
+@click.option('--report-subject')
+@click.option('--report-file', type=click.File('r', encoding='utf-8'))
+@click.option('--report-format')
+@click.option('--orphaned-subject')
+@click.option('--orphaned-file', type=click.File('r', encoding='utf-8'))
+@click.option('--closed-subject')
+@click.option('--closed-file', type=click.File('r', encoding='utf-8'))
+@click.pass_context
+def update_tenant(ctx, name, yamlfile, admin, charset, enable, disable,
+                  days_to_close, days_to_orphan,
+                  ml_name_format, new_ml_account,
+                  welcome_file, readme_file, remove_file, reopen_file,
+                  goodbye_file,
+                  report_subject, report_file, report_format,
+                  orphaned_subject, orphaned_file,
+                  closed_subject, closed_file):
+    tenant_config = {}
+    if yamlfile:
+        tenant_config = yaml.load(yamlfile.read())
+    if len(admin) > 0:
+        tenant_config['admins'] = set(admin)
+    if charset is not None:
+        tenant_config['charset'] = charset
+    if enable is True:
+        tenant_config['status'] = const.TENANT_STATUS_ENABLED
+    elif disable is True:
+        tenant_config['status'] = const.TENANT_STATUS_DISABLED
+    if days_to_close is not None:
+        tenant_config['days_to_close'] = days_to_close
+    if days_to_orphan is not None:
+        tenant_config['days_to_orphan'] = days_to_orphan
+    if ml_name_format is not None:
+        tenant_config['ml_name_format'] = ml_name_format
+    if new_ml_account is not None:
+        tenant_config['new_ml_account'] = new_ml_account
+    if welcome_file is not None:
+        tenant_config['welcome_msg'] = welcome_file.read()
+    if readme_file is not None:
+        tenant_config['readme_msg'] = readme_file.read()
+    if remove_file is not None:
+        tenant_config['remove_msg'] = remove_file.read()
+    if goodbye_file is not None:
+        tenant_config['goodbye_msg'] = goodbye_file.read()
+    if reopen_file is not None:
+        tenant_config['reopen_msg'] = reopen_file.read()
+    if report_subject is not None:
+        tenant_config['report_subject'] = report_subject
+    if report_file is not None:
+        tenant_config['report_msg'] = report_file.read()
+    if report_format is not None:
+        tenant_config['report_format'] = report_format
+    if orphaned_subject is not None:
+        tenant_config['orphaned_subject'] = orphaned_subject
+    if orphaned_file is not None:
+        tenant_config['orphaned_msg'] = orphaned_file.read()
+    if closed_subject is not None:
+        tenant_config['closed_subject'] = closed_subject
+    if closed_file is not None:
+        tenant_config['closed_msg'] = closed_file.read()
+
+    logging.debug("tenant_name: %s", name)
+    logging.debug("tenant_config: %s", tenant_config)
+    config = ctx.obj['config']
+    db.init_db(config['db_url'],  config['db_name'])
+    db.update_tenant(name, "CLI", **tenant_config)
+    return 0
+
+
+@tenant.command('show', help='Show parameters of a tenant')
+@click.argument('name')
+@click.pass_context
+def show_tenant(ctx, name):
+    config = ctx.obj['config']
+    db.init_db(config['db_url'],  config['db_name'])
+    tenant = db.get_tenant(name)
+    if tenant is None:
+        logging.error("tenant %s not found", name)
+        return 1
+    for key in ['_id', 'logs']:
+        if key in tenant:
+            del tenant[key]
+    print(yaml.dump(tenant, allow_unicode=True, line_break='|'))
+    return 0
+
+
+@tenant.command('list', help='List tenants')
+@click.pass_context
+def list_tenant(ctx):
+    config = ctx.obj['config']
+    db.init_db(config['db_url'],  config['db_name'])
+    tenants = db.find_tenants({})
+    for tenant in tenants:
+        print("%(tenant_name)s: %(status)s %(created)s" % tenant)
+    return 0
+
+
+@tenant.command('delete', help='Delete a tenant')
+@click.argument('name')
+@click.pass_context
+def delete_tenant(ctx, name):
+    config = ctx.obj['config']
+    db.init_db(config['db_url'],  config['db_name'])
+    tenant = db.get_tenant(name)
+    if tenant is None:
+        logging.error("tenant %s not found", name)
+        return 1
+    db.delete_tenant(name)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(cli(obj={}))
---
+++ tempml/tests/test_ctl.py
@@ -0,0 +1,209 @@
+# Copyright 2017 by Akira Yoshiyama <akirayoshiyama@gmail.com>.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+Smoketests for reviewer (tempml.cmd.reviewer)
+"""
+
+import click
+from click.testing import CliRunner
+from datetime import datetime
+import email
+import logging
+from os.path import dirname, join
+import random
+import smtpd
+import tempfile
+import time
+import unittest
+from unittest import mock
+import yaml
+
+from tempml import const
+from tempml.tests import fake_db
+from tempml import log
+
+
+log.setup(debug=True)
+
+
+ML_NAME = "test-%06d"
+
+
+class DummySMTPServer(object):
+    def __init__(self, localaddr, localport, **kwargs):
+        pass
+
+
+class DummySMTPClient(object):
+    def __init__(self, host, port):
+        pass
+
+    def set_debuglevel(self, value):
+        pass
+
+    def sendmail(self, _from, members, message):
+        pass
+
+    def quit(self):
+        pass
+
+
+class NotifyTest(unittest.TestCase):
+    """notify() tests"""
+
+    @mock.patch('tempml.db', fake_db)
+    def setUp(self):
+        from tempml.cmd import ctl
+        self.ctl = ctl
+        self.tester = lambda *x: CliRunner().invoke(self.ctl.cli, x)
+        self.db_name = "test%04d" % random.randint(0, 1000)
+        self.ml_name_arg = None
+        self.content_arg = None
+        self.members_arg = None
+
+        fake_db.init_db("", self.db_name)
+        self.tenant_name = "tenant1"
+        self.config = {
+            "admins": {"hoge"},
+            "charset": "iso-2022-jp",
+            "ml_name_format": "ml-%06d",
+            "new_ml_account": "new",
+            "days_to_close": 7,
+            "days_to_orphan": 7,
+            "welcome_msg": "welcome_msg",
+            "readme_msg": "readme_msg",
+            "remove_msg": "remove_msg",
+            "reopen_msg": "reopen_msg",
+            "goodbye_msg": "goodbye_msg",
+            "report_subject": "report_subject",
+            "report_msg": "report_msg",
+            "report_format": "report_format",
+            "orphaned_subject": "orphaned_subject",
+            "orphaned_msg": "orphaned_msg",
+            "closed_subject": "closed_subject",
+            "closed_msg": "closed_msg",
+        }
+        fake_db.create_tenant(self.tenant_name, "hoge", self.config)
+
+        self.ctx = mock.MagicMock()
+        self.ctx.obj = dict(
+            config=dict(
+                 db_name=self.db_name, db_url="mongodb://localhost/",
+                 relay_host="localhost", relay_port=25,
+                 listen_address="192.168.0.1", listen_port=25,
+                 log_file="/var/log/tempml.log", domain="example.com"),
+            debug=False)
+
+    def tearDown(self):
+        fake_db.clear_db()
+
+    def test_list_tenant(self):
+        result = self.tester(
+            "--config-file", "sample/tempml.conf", "tenant", "list")
+        self.assertEqual(result.exit_code, 0)
+        self.assertTrue(result.output.startswith(self.tenant_name))
+
+    def test_show_tenant(self):
+        result = self.tester(
+            "--config-file", "sample/tempml.conf", "tenant", "show",
+            self.tenant_name)
+        self.assertEqual(result.exit_code, 0)
+        config = yaml.load(result.output)
+        for key, value in self.config.items():
+            self.assertEqual(config[key], value)
+
+    def test_update_tenant(self):
+        result = self.tester(
+            "--config-file", "sample/tempml.conf", "tenant", "update",
+            "--days-to-orphan", "14",
+            self.tenant_name)
+        self.assertEqual(result.output, "")
+        self.assertEqual(result.exit_code, 0)
+        config = fake_db.get_tenant(self.tenant_name)
+        self.assertEqual(config["days_to_orphan"], 14)
+
+    def test_delete_tenant(self):
+        result = self.tester(
+            "--config-file", "sample/tempml.conf", "tenant", "delete",
+            self.tenant_name)
+        self.assertEqual(result.exit_code, 0)
+
+    def test_create_tenant_by_yaml(self):
+        fake_db.clear_db()
+        with tempfile.NamedTemporaryFile(mode="wt") as t:
+            logging.debug("TempFile: %s", t.name)
+            yaml.dump(self.config, t)
+            result = self.tester(
+                "--config-file", "sample/tempml.conf", "tenant", "create",
+                self.tenant_name, "--yamlfile", t.name)
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(result.output, "")
+        config = fake_db.get_tenant(self.tenant_name)
+        for key, value in self.config.items():
+            self.assertEqual(config[key], value)
+
+    def test_create_tenant_by_opt(self):
+        fake_db.clear_db()
+        with tempfile.NamedTemporaryFile(mode="wt") as t1, \
+                tempfile.NamedTemporaryFile(mode="wt") as t2, \
+                tempfile.NamedTemporaryFile(mode="wt") as t3, \
+                tempfile.NamedTemporaryFile(mode="wt") as t4, \
+                tempfile.NamedTemporaryFile(mode="wt") as t5, \
+                tempfile.NamedTemporaryFile(mode="wt") as t6, \
+                tempfile.NamedTemporaryFile(mode="wt") as t7, \
+                tempfile.NamedTemporaryFile(mode="wt") as t8:
+            t1.write(self.config["welcome_msg"])
+            t2.write(self.config["readme_msg"])
+            t3.write(self.config["remove_msg"])
+            t4.write(self.config["reopen_msg"])
+            t5.write(self.config["goodbye_msg"])
+            t6.write(self.config["report_msg"])
+            t7.write(self.config["orphaned_msg"])
+            t8.write(self.config["closed_msg"])
+            t1.seek(0)
+            t2.seek(0)
+            t3.seek(0)
+            t4.seek(0)
+            t5.seek(0)
+            t6.seek(0)
+            t7.seek(0)
+            t8.seek(0)
+            result = self.tester(
+                "--config-file", "sample/tempml.conf", "tenant", "create",
+                self.tenant_name,
+                "--admin", "hoge",
+                "--charset", self.config["charset"],
+                "--days-to-close", self.config["days_to_close"],
+                "--days-to-orphan", self.config["days_to_orphan"],
+                "--ml-name-format", self.config["ml_name_format"],
+                "--new-ml-account", self.config["new_ml_account"],
+                "--welcome-file", t1.name,
+                "--readme-file", t2.name,
+                "--remove-file", t3.name,
+                "--reopen-file", t4.name,
+                "--goodbye-file", t5.name,
+                "--report-subject", self.config["report_subject"],
+                "--report-file", t6.name,
+                "--report-format", self.config["report_format"],
+                "--orphaned-subject", self.config["orphaned_subject"],
+                "--orphaned-file", t7.name,
+                "--closed-subject", self.config["closed_subject"],
+                "--closed-file", t8.name)
+        self.assertEqual(result.output, "")
+        self.assertEqual(result.exit_code, 0)
+        config = fake_db.get_tenant(self.tenant_name)
+        for key, value in self.config.items():
+            self.assertEqual(config[key], value)
